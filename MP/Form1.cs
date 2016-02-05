28be31c741cd2a6c3e635ebd6c31312af7727ff9
Abdelrahman Mansour <abdo_mans@live.com>
2016-02-05 21:54:00 +0200

    GUI for minimum spanning tree
    Gui for getting minimum spanning tree for a connected graph in c# using
    prime;s algorithm

---
+++ MP/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP
+{
+    public partial class Form1 : Form
+    {
+        Nodes n;
+        int V;
+        int[,] graph = new int[10, 10];
+        List<int> l = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+            panel2.Refresh();
+            l.Clear();
+            n = new Nodes(panel1,panel2);
+           // textBox1.Text = "5";
+            V = Convert.ToInt32(textBox1.Text);
+            n.draw(Convert.ToInt32(textBox1.Text));
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                comboBox1.Items.Add(i);
+                comboBox2.Items.Add(i);
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == comboBox2.Text || textBox2.Text == "")
+            {
+                MessageBox.Show("Error");
+            }
+            else
+            {
+
+                n.Draw_edge(Convert.ToInt32(comboBox1.Text), Convert.ToInt32(comboBox2.Text), textBox2.Text);
+                graph[Convert.ToInt32(comboBox1.Text), Convert.ToInt32(comboBox2.Text)] = Convert.ToInt32(textBox2.Text);
+                graph[Convert.ToInt32(comboBox2.Text), Convert.ToInt32(comboBox1.Text)] = Convert.ToInt32(textBox2.Text);
+            
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            primMST(graph);
+        }
+        int min_node_value(int[] node_value, bool[] visited)
+        {
+            // Initialize min value
+            int min = Int32.MaxValue;
+            int min_index=-1;
+
+            for (int v = 0; v < V; v++)
+                if (visited[v] == false && node_value[v] < min)
+                {
+                    min = node_value[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+       
+        void primMST(int [,]graph)
+        {
+            int[] parent = new int[V]; // Array to store constructed MST
+            int[] node_value = new int[V];   // node_value values used to pick minimum weight edge
+            bool[] visited = new bool[V];  
+
+           
+            for (int i = 0; i < V; i++)
+            {
+                node_value[i] = Int32.MaxValue;
+                visited[i] = false;
+            }
+            node_value[0] = 0;     //first index
+            parent[0] = -1; //first node is root
+
+            
+            for (int count = 0; count < V ; count++)
+            {
+                int u = min_node_value(node_value, visited);
+                visited[u] = true;
+                l.Add(u);
+                for (int v = 0; v < V; v++)
+                    if (graph[u, v] != 0 && visited[v] == false && graph[u, v] < node_value[v])
+                    {
+                        parent[v] = u; 
+                        node_value[v] = graph[u, v];  
+                    }
+            }
+            int total_cost=0;
+           for ( int i = 1; i < V; i++)
+           {
+               string s = "";
+               total_cost+=graph[i,parent[i]];
+               n.draw_sol(parent[i],i,graph[i,parent[i]]);
+           }
+           textBox3.Text = total_cost.ToString();
+           textBox4.Text = string.Join(" , ",l.ToArray());
+        }
+    }
+}
---
+++ MP/Nodes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP
+{
+    class Nodes
+    {
+        public Graphics formGraphics,formGraphics2;
+         public Point[] points =    //Nodes points
+             {
+                   new Point(70,150),     //0     
+                   new Point(150,50),    //1     
+                   new Point(250,50),    //2  
+                   new Point(350,50),    //3
+                   new Point(430,150),    //4  
+                   new Point(350,250),    //5 
+                   new Point(250,250),    //6  
+                   new Point(150,250),    //7
+                   new Point(250,150),    //8
+                   new Point(250,50),    //9
+             };
+         public Point[] points_edge =    //Nodes points
+             {
+              
+                   new Point(90,160),     //0     
+                   new Point(170,70),    //1     
+                   new Point(270,70),    //2  
+                   new Point(370,70),    //3
+                   new Point(450,170),    //4  
+                   new Point(370,270),    //5 
+                   new Point(270,270),    //6  
+                   new Point(170,270),    //7
+                   new Point(270,170),    //8
+                   new Point(270,70),    //9
+             };
+        public Nodes(Panel panel1,Panel panel2)
+        {
+            formGraphics = panel1.CreateGraphics();
+            formGraphics.DrawRectangle(new Pen(Color.Blue, 5), panel1.ClientRectangle);
+            formGraphics2 = panel2.CreateGraphics();
+            formGraphics2.DrawRectangle(new Pen(Color.Blue, 5), panel2.ClientRectangle);
+        }
+
+        public void draw_sol(int A,int B,int s)
+        {
+            SolidBrush br = new SolidBrush(Color.Black);
+            formGraphics2.FillEllipse(br, new Rectangle(points[A], new Size(40, 40)));
+            formGraphics2.DrawString(A.ToString(), new Font("Arial", 18), Brushes.White, new Point(points[A].X + 10, points[A].Y + 5));
+            formGraphics2.FillEllipse(br, new Rectangle(points[B], new Size(40, 40)));
+            formGraphics2.DrawString(B.ToString(), new Font("Arial", 18), Brushes.White, new Point(points[B].X + 10, points[B].Y + 5));
+            formGraphics2.DrawString(s.ToString(), new Font("Arial", 10), Brushes.Black, new Point((points[A].X + points[B].X) / 2, (points[A].Y + points[B].Y) / 2));
+            formGraphics2.DrawLine(new Pen(Color.Orange, 3), points_edge[A], points_edge[B]);
+        }
+
+        public void draw(int Node_number)
+        {
+          for (int i = 0; i < Node_number; i++)
+            {
+                SolidBrush br = new SolidBrush(Color.Black);
+                formGraphics.FillEllipse(br, new Rectangle(points[i], new Size(40, 40)));
+                formGraphics.DrawString(i.ToString(), new Font("Arial", 18), Brushes.White, new Point(points[i].X + 10, points[i].Y + 5));
+            }
+           // formGraphics.Dispose();
+        }
+
+        public void Draw_edge(int A,int B,string s)
+        {
+            formGraphics.DrawString(s, new Font("Arial", 10), Brushes.Black, new Point((points[A].X + points[B].X)/2,(points[A].Y + points[B].Y) /2));
+            formGraphics.DrawLine(new Pen(Color.Orange,3),points_edge[A],points_edge[B]);
+        }
+    }
+}
